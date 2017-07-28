6c7b9a66b52272bd44703f04f861dd58b2b110e6
Elie Schramm <eschramm@wustl.edu>
2017-07-28 15:50:21 -0500

    first commit of spring service

---
+++ GetCourses/GetCourses/src/main/java/org/ratemycourse/GetCourses/GetCoursesApplication.java
@@ -0,0 +1,12 @@
+package org.ratemycourse.GetCourses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetCoursesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetCoursesApplication.class, args);
+	}
+}
---
+++ GetCourses/GetCourses/src/test/java/org/ratemycourse/GetCourses/GetCoursesApplicationTests.java
@@ -0,0 +1,16 @@
+package org.ratemycourse.GetCourses;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GetCoursesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
